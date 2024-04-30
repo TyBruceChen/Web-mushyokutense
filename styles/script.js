ea8c7cc8b66b74d087de24b87375d993249a0f5f
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-04-29 22:16:42 -0700

    Add files via upload

--- styles/script.js
+++ styles/script.js
@@ -9,6 +9,7 @@ function videoInit(){
     video.volume = '0.75'
     video.style.opacity = '0.7'
     video.style.width = '100%'
+    video.style.display = 'block'
     video.setAttribute('id','mushyokutense-video')
 
     videosrc.src = "static/mushyokutense.mp4"
@@ -36,13 +37,15 @@ function videoControl(){
 function videoVoice(){
     const video = document.getElementById('mushyokutense-video')
     const button2 = document.getElementById('videoVoice')
-   if (video.muted == 'Mute'){
-    video.muted = !video.muted
-    button2.textContent = 'Unmute background music'
+   if (video.muted == true){
+    video.muted = false
+    button2.textContent = 'Mute background music'
+    console.log(video.muted)
    }
    else{
-    video.muted = !video.muted
-    button2.textContent = 'Mute background music'
+    video.muted = true
+    button2.textContent = 'Unmute background music'
+    console.log(video.muted)
    }
 }
 
@@ -50,5 +53,6 @@ document.addEventListener('DOMContentLoaded', function(){
     //load functions
     videoInit()
     console.log(document.getElementById('mushyokutense-video').style.display)
+    document.getElementById('mushyokutense-video').muted = false
 })
 
